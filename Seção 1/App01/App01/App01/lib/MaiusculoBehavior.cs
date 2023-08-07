50100eee63fb2399c053b4032e9a8474970c23cb
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-08-06 23:26:26 -0300

    Até a aula 4

---
+++ Seção 1/App01/App01/App01/lib/MaiusculoBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App01.lib
+{
+    public class MaiusculoBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.TextChanged += ColocarEmMaiusculo;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            bindable.TextChanged -= ColocarEmMaiusculo;
+        }
+
+        private void ColocarEmMaiusculo(object sender, TextChangedEventArgs e)
+        {
+            Entry entry = (Entry)sender;
+
+            if (!string.IsNullOrEmpty(e.NewTextValue)) 
+            {
+                entry.Text = e.NewTextValue.ToUpper();
+            }
+        }
+    }
+}
