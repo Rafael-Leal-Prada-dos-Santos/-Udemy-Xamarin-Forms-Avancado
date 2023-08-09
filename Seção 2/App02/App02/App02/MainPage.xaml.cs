daafaa1c2a23346523f00bd8c37582a199fcbc7e
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-08-08 23:42:47 -0300

    Seção 2 Quase Finalizada

--- Seção 2/App02/App02/App02/MainPage.xaml.cs
+++ Seção 2/App02/App02/App02/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -13,6 +14,58 @@ namespace App02
         public MainPage()
         {
             InitializeComponent();
+
+            ListaFuncionarios.ItemsSource = ObterFuncionarios();
+        }
+
+        private List<Grupo> ObterFuncionarios()
+        {
+            return new List<Grupo>()
+            {
+
+                new Grupo("Presidentes", "CEO", "Presidente da empresa")
+                {
+                    new Pessoa(){ Nome = "Rafael"},
+                    new Pessoa(){ Nome = "Renato"},
+                },
+                new Grupo("Diretores", "Dir.", "Diretor da empresa")
+                {
+                    new Pessoa(){ Nome = "Richard"},
+                    new Pessoa(){ Nome = "Brenno"},
+                },
+                 new Grupo("Gerentes", "Ger.", "Gerente da empresa")
+                {
+                    new Pessoa(){ Nome = "Ivaldo"},
+                    new Pessoa(){ Nome = "Luiz"}
+                },
+                new Grupo("Colaboradores", "Colab.", "Colaboradores da empresa")
+                {
+                    new Pessoa(){ Nome = "Daniel"},
+                    new Pessoa(){ Nome = "Debora"},
+                    new Pessoa(){ Nome = "Alex"},
+                    new Pessoa(){ Nome = "Vander"},
+                    new Pessoa(){ Nome = "Milton"},
+                }
+            };
+        }
+
+        public class Grupo : List<Pessoa> {
+            public string Titulo { get; set; }
+            public string TituloCurto { get; set; }
+            public string Descricao { get; set; }
+
+            public Grupo(string titulo, string tituloCurto, string descricao) 
+            {
+                this.Titulo = titulo;
+                this.TituloCurto = tituloCurto;
+                this.Descricao = descricao;
+            }
+        }
+
+
+        public class Pessoa 
+        {
+            public string Nome { get; set; }
         }
     }
 }
