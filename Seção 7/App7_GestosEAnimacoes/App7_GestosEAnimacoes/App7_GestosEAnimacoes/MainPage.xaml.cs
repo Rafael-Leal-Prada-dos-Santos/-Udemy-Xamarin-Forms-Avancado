7be81537b644966f1d833c7639baae6711b326bb
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-08-13 23:15:58 -0300

    Commit seção 7 Completa

--- Seção 7/App7_GestosEAnimacoes/App7_GestosEAnimacoes/App7_GestosEAnimacoes/MainPage.xaml.cs
+++ Seção 7/App7_GestosEAnimacoes/App7_GestosEAnimacoes/App7_GestosEAnimacoes/MainPage.xaml.cs
@@ -15,12 +15,36 @@ namespace App7_GestosEAnimacoes
         public MainPage()
         {
             InitializeComponent();
+
+            PanGestureRecognizer pan = new PanGestureRecognizer();
+            pan.PanUpdated += PanGestureRecognizer_Tapped;
+
+            MinhaLabel.GestureRecognizers.Add(pan);
         }
 
         private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
         {
             count++;
-            DisplayAlert("Toque", $"Label tocada: {count}", "OK");
+            //DisplayAlert("Toque", $"Label tocada: {count}", "OK");
+
+            //MeuBoxView.TranslateTo(200, 350, 1000, Easing.BounceOut);
+            //MeuBoxView.ScaleTo(2, 1000);
+            //MeuBoxView.FadeTo(0.5, 1000);
+            //MeuBoxView.RotateTo(45, 1000, Easing.SpringOut);
+
+            Animation animacao = new Animation(v => MeuBoxView.WidthRequest = v, 50, 100);
+            animacao.Commit(this, "animacao", 16, 1000);
+        }
+
+        private void PanGestureRecognizer_Tapped(object sender, PanUpdatedEventArgs e)
+        {
+            if (e.StatusType == GestureStatus.Running) 
+            {
+                Rectangle rect = new Rectangle(e.TotalX, e.TotalY, 200, 25);
+
+                AbsoluteLayout.SetLayoutBounds(MinhaLabel, rect);
+                AbsoluteLayout.SetLayoutFlags(MinhaLabel, AbsoluteLayoutFlags.None);
+            }
         }
 
         //gesture
