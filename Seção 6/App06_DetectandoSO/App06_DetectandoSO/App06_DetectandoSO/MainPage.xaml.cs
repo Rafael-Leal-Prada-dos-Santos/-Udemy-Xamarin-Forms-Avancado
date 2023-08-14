03971198c02a563c9c9b4abbaa356080ab7a2f36
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-08-13 21:05:11 -0300

    Seção 6 finalizada

---
+++ Seção 6/App06_DetectandoSO/App06_DetectandoSO/App06_DetectandoSO/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App06_DetectandoSO
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (Device.RuntimePlatform == TargetPlatform.iOS.ToString()) { 
+                Container.Margin = new Thickness(0, 10, 0, 0);
+            }
+
+            if (Device.Idiom == TargetIdiom.Tablet) 
+            {
+                Container.BackgroundColor = Color.Aqua;
+            }
+
+            /*
+                 Device.OnPlatform(
+                    iOS: () => {
+                        Margin = new Thickness(0,10,0,0);
+                     },
+                    Android: () => {
+            
+                    },
+                    WinPhone: () =>{
+                    }
+                  );
+             */
+
+        }
+    }
+}
