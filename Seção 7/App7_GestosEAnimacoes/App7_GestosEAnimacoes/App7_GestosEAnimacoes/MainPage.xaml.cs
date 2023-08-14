6862599566c530003d03a3693b871a8f8484bd40
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-08-13 21:18:33 -0300

    Início seção 7

---
+++ Seção 7/App7_GestosEAnimacoes/App7_GestosEAnimacoes/App7_GestosEAnimacoes/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App7_GestosEAnimacoes
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            count++;
+            DisplayAlert("Toque", $"Label tocada: {count}", "OK");
+        }
+
+        //gesture
+        //Tap - Toque
+        //Pinch - Pinça - Galeria de imagens zoom
+        //Pan - Arastar
+    }
+}
