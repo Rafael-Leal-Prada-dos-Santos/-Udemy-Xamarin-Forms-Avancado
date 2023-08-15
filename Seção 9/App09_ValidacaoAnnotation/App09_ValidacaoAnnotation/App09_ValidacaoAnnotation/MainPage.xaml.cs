b8387c9dceefae9a0b2a9dab36baf2b2c346b2fa
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-08-15 13:38:22 -0300

    Aulas 43,44,45,46

--- Seção 9/App09_ValidacaoAnnotation/App09_ValidacaoAnnotation/App09_ValidacaoAnnotation/MainPage.xaml.cs
+++ Seção 9/App09_ValidacaoAnnotation/App09_ValidacaoAnnotation/App09_ValidacaoAnnotation/MainPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using App09_ValidacaoAnnotation.Modelo;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,43 @@ namespace App09_ValidacaoAnnotation
         public MainPage()
         {
             InitializeComponent();
+
+
+            BtnSalvar.Clicked += delegate
+            {
+
+                Pessoa pessoa = new Pessoa()
+                {
+                    Nome = TxtNome.Text,
+                    Email = TxtEmail.Text,
+                    CPF = TxtCPF.Text,
+                };
+
+                List<ValidationResult> resultadosValidacoes = new List<ValidationResult>();
+
+                ValidationContext contexto = new ValidationContext(pessoa);
+
+                bool ehValido = Validator.TryValidateObject(pessoa, contexto, resultadosValidacoes);
+
+                if (!ehValido)
+                {
+                    if (resultadosValidacoes.Count > 0)
+                    {
+                        LblMSG.Text = string.Empty;
+
+                        foreach (ValidationResult vr in resultadosValidacoes)
+                        {
+                            LblMSG.Text += $"{string.Format(vr.ErrorMessage, vr.MemberNames)}\n";
+                        }
+                    }
+                }
+                else 
+                {
+                    LblMSG.Text = "OK";
+                    LblMSG.TextColor = Color.Green;
+                }
+            };
+
         }
     }
 }
---
+++ Seção 9/App09_ValidacaoAnnotation/App09_ValidacaoAnnotation/App09_ValidacaoAnnotation/Modelo/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace App09_ValidacaoAnnotation.Modelo
+{
+    public class Pessoa
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "O Campo nome é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessageResourceName = "MSG_E01", ErrorMessageResourceType = typeof(Lingua.Mensagem))]
+        public string Email { get; set; }
+
+        [Required(ErrorMessageResourceName = "MSG_E01", ErrorMessageResourceType = typeof(Lingua.Mensagem))]
+        public string CPF { get; set; }
+    }
+}
