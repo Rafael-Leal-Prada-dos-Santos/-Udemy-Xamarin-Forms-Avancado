74ca28fb087a34c09a6b1a32dad0328a15e01f81
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-08-15 00:38:09 -0300

    Início seção 8

---
+++ Seção 8/App08_MultiIdiomas/App08_MultiIdiomas/App08_MultiIdiomas/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App08_MultiIdiomas
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Lang.AppLang.Culture = new System.Globalization.CultureInfo("pt-PT");
+            LblMsg.Text = Lang.AppLang.MSG_01;
+        }
+    }
+}
