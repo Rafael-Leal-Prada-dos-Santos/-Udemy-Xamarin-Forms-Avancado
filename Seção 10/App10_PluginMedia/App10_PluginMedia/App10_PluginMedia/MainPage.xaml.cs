15a053cf2d36e5f535cee85d67c4ccc1564422d0
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-08-17 23:07:29 -0300

    Commit final seção 10

--- Seção 10/App10_PluginMedia/App10_PluginMedia/App10_PluginMedia/MainPage.xaml.cs
+++ Seção 10/App10_PluginMedia/App10_PluginMedia/App10_PluginMedia/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.Media;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -13,6 +14,34 @@ namespace App10_PluginMedia
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                DisplayAlert("Sem camera", "Câmera indisponível.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "teste",
+                Name = "foto.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            await DisplayAlert("File Location", file.Path, "OK");
+
+            Imagem.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                return stream;
+            });
         }
     }
 }
