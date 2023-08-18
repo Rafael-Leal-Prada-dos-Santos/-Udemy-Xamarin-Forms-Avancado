9a8c4891e3d398f70e4c291bd6cec6903d898270
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-08-18 13:44:11 -0300

    Seção 11 início

---
+++ Seção 11/App11_ArmazenamentoEBancoDeDados/App11_ArmazenamentoEBancoDeDados/App11_ArmazenamentoEBancoDeDados/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using PCLStorage;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App11_ArmazenamentoEBancoDeDados
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Escrever_Clicado(object sender, EventArgs e)
+        {
+            Util.GerenciadorArmazenamento.SalvarArquivo("arquivo.txt", txtConteudo.Text);
+        }
+
+        private async void LerArquivo_Clicado(object sender, EventArgs e)
+        {
+           string conteudo = await  Util.GerenciadorArmazenamento.LerArquivo("arquivo.txt");
+
+           lblConteudoArquivo.Text = conteudo;
+        }
+    }
+}
