383045b2fb37e21efe38e2799ff60e83239d04e9
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-08-21 00:23:29 -0300

    Inicio seção 12

---
+++ Seção 12/App12_FrameworkPrism/App12_FrameworkPrism/App12_FrameworkPrism/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using App12_FrameworkPrism.ViewModels;
+using App12_FrameworkPrism.Views;
+using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App12_FrameworkPrism
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer inicializador = null) : base(inicializador) 
+        {
+
+        }
+
+        protected override void OnInitialized()
+        {
+            //Qual a primeira página
+            InitializeComponent();
+
+            NavigationService.NavigateAsync($"NavigationPage/{nameof(LoginPage)}");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //Registrar todas  as views
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<LoginPage, LoginPageViewModel>();
+        }
+    }
+}
