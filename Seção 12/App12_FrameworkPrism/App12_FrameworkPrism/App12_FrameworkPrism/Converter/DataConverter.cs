53c95bc62597845b7d4ee28fd6064732268d49ac
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-08-26 16:01:51 -0300

    Final seção 12

---
+++ Seção 12/App12_FrameworkPrism/App12_FrameworkPrism/App12_FrameworkPrism/Converter/DataConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App12_FrameworkPrism.Converter
+{
+    public class DataConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTimeOffset dt = (DateTimeOffset)value;
+
+            return dt.ToString("dd/MM/yyyy");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
