2ad430f923089415d9b32acb8b6b73a99dce4577
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-08-27 23:38:10 -0300

    Início da seção 14 OAuth

---
+++ Seção 14/App14_LoginSocialOAuth/App14_LoginSocialOAuth/App14_LoginSocialOAuth/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Ioc;
+using Prism;
+using Prism.Unity;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using App14_LoginSocialOAuth.Views;
+using App14_LoginSocialOAuth.ViewModels;
+
+namespace App14_LoginSocialOAuth
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer inicializador = null) : base(inicializador)
+        {
+
+        }
+
+        protected override void OnInitialized()
+        {
+            //Qual a primeira página
+            InitializeComponent();
+
+            //NavigationService.NavigateAsync($"NavigationPage/{nameof(ListaProfissionais)}");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //Registrar todas  as views
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<PaginaInicial, PaginaInicialViewModel>();
+        }
+
+
+    }
+}
---
+++ Seção 14/App14_LoginSocialOAuth/App14_LoginSocialOAuth/App14_LoginSocialOAuth/Views/PaginaInicial.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace App14_LoginSocialOAuth.Views
+{
+    public partial class PaginaInicial : ContentPage
+    {
+        public PaginaInicial()
+        {
+            InitializeComponent();
+        }
+    }
+}
