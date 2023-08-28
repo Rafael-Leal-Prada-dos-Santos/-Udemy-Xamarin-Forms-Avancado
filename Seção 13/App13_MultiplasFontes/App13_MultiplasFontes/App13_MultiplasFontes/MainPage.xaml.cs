7959eee8870bc84918d5bcccc6eac6e607aa92fd
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-08-27 22:20:29 -0300

    Seção 13 iniciada e finalizada

---
+++ Seção 13/App13_MultiplasFontes/App13_MultiplasFontes/App13_MultiplasFontes/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App13_MultiplasFontes
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            lblBemVindo.FontFamily = 
+                Device.OnPlatform("lourod", "lourod.ttf#lourod", "Assets/Fonts/lourod.ttf#lourod") ;
+        }
+    }
+}
