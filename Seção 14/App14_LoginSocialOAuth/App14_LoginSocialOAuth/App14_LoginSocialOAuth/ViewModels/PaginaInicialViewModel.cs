0c375e0bd1cf35b0d9faeabcc756372a95510cdc
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-08-28 14:02:11 -0300

    Commit inicio do projeto

--- Seção 14/App14_LoginSocialOAuth/App14_LoginSocialOAuth/App14_LoginSocialOAuth/ViewModels/PaginaInicialViewModel.cs
+++ Seção 14/App14_LoginSocialOAuth/App14_LoginSocialOAuth/App14_LoginSocialOAuth/ViewModels/PaginaInicialViewModel.cs
@@ -8,9 +8,23 @@ namespace App14_LoginSocialOAuth.ViewModels
 {
     public class PaginaInicialViewModel : BindableBase
     {
+        private string _title;
+        public string Title 
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public DelegateCommand ComandoLogarNoFacebook { get; set; }
+
         public PaginaInicialViewModel()
         {
+            ComandoLogarNoFacebook = new DelegateCommand(LogarNoFacebook);
+        }
 
+        private void LogarNoFacebook()
+        {
+            throw new NotImplementedException();
         }
     }
 }
